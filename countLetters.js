76de3dd32259a0d5aa8e143f786570ee2c06be9f
Avivit Weissman <wavivit@gmail.com>
2021-04-28 21:31:48 +0000

    Almost final version of countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,17 @@
 const countLetters = function(sentence) {
+  let result = {};
+  for (const letter of sentence) {
+    if (letter !== " "){
+      if(result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+    
+  }
 
+  return result;
 
 };
 
@@ -18,14 +30,20 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const string1 = "lighthouse in the house";
-assertEqual(countLetters(string1['l']),1);
-assertEqual(countLetters(string1['i']),2);
-assertEqual(countLetters(string1['g']),1);
-assertEqual(countLetters(string1['h']),4);
-assertEqual(countLetters(string1['t']),2);
-assertEqual(countLetters(string1['o']),2);
-assertEqual(countLetters(string1['u']),2);
-assertEqual(countLetters(string1['s']),2);
-assertEqual(countLetters(string1['e']),3);
-assertEqual(countLetters(string1['n']),1);
+const string1 = countLetters("lighthouse in the house");
+assertEqual(string1['l'],1);
+assertEqual(string1['i'],2);
+assertEqual(string1['g'],1);
+assertEqual(string1['h'],4);
+assertEqual(string1['t'],2);
+assertEqual(string1['o'],2);
+assertEqual(string1['u'],2);
+assertEqual(string1['s'],2);
+assertEqual(string1['e'],3);
+assertEqual(string1['n'],1);
+
+const string2 = countLetters('avivit');
+assertEqual(string2['a'], 1);
+assertEqual(string2['v'], 2);
+assertEqual(string2['i'], 2);
+assertEqual(string2['t'], 1);
