b16914d85961a83da63f29e4de063410373d9ef2
Avivit Weissman <wavivit@gmail.com>
2021-04-28 21:32:59 +0000

    Final version of countLetters after eslint fixies

--- countLetters.js
+++ countLetters.js
@@ -1,8 +1,8 @@
 const countLetters = function(sentence) {
   let result = {};
   for (const letter of sentence) {
-    if (letter !== " "){
-      if(result[letter]) {
+    if (letter !== " ") {
+      if (result[letter]) {
         result[letter] += 1;
       } else {
         result[letter] = 1;
