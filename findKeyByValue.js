c25eecb7babfeac07cc7a431d46c286fc1ae98ab
Avivit Weissman <wavivit@gmail.com>
2021-04-28 22:55:45 +0000

    first draft of findByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,14 @@
+const findKeyByValue = function(object, value) {
+
+}
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed:✅✅✅ " +  actual + "  ===  " + expected);
+  } else {
+    console.log(`Assertion Failed:⛔️⛔️⛔️  ${actual} !==  ${expected}`);
+  }
+
+};
