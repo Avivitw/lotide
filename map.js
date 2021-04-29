5e4984b118a441d3976af9f705152eb564e5a8be
Avivit Weissman <wavivit@gmail.com>
2021-04-29 22:19:53 +0000

    finished version of map

--- map.js
+++ map.js
@@ -1,11 +1,11 @@
-const map = function(array,callback){
+const map = function(array,callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
 
   return results;
-}
+};
 
 
 const eqArrays = function(arr1, arr2) {
@@ -25,7 +25,7 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(actual, expected) {
   if (!eqArrays(actual, expected)) {
-    console.log(`Assertion Failed:⛔️⛔️⛔️  ${actual} !==  ${expected}`); 
+    console.log(`Assertion Failed:⛔️⛔️⛔️  ${actual} !==  ${expected}`);
   } else {
     console.log(`Assertion Passed:✅✅✅  ${actual}  ===  ${expected}`);
    
