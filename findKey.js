25e2d8fc73b1096a2d360e0dad1c079451fbcf35
Avivit Weissman <wavivit@gmail.com>
2021-04-30 01:03:11 +0000

    final version of the function that works with test cases

--- findKey.js
+++ findKey.js
@@ -5,13 +5,15 @@ const assertEqual = function(actual, expected) {
     console.log("Assertion Passed:🙆✅✅ " +  actual + "  ===  " + expected);
   } else {
     console.log(`Assertion Failed:🙅⛔️⛔️  ${actual} !==  ${expected}`);
-  };
+  }
+};
 
 // ---------- FUNCTION IMPLEMENTATION ---------- //
 const findKey = function(object, callback) {
   //create a loop and search for the key with the value help
   for (const key in object) {
-    if (object[key][callback] === value) {
+  //get the value by invoking the callback function
+    if (callback(object[key])) {
       return key;
     }
   }
@@ -28,3 +30,11 @@ assertEqual(findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  'a': {val: 3},
+  'b': {val: 4},
+  'c': {val: 5},
+  'd': {val: 6},
+  'e': {val: 7},
+}, x => x.val === 6), "d");
