55e3213397ec9d1513a618618e3118a1800da61f
Avivit Weissman <wavivit@gmail.com>
2021-05-03 20:24:00 +0000

    removed the tests and export module tail

--- tail.js
+++ tail.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:✅✅✅  ${actual} ===  ${expected}`);
-  } else {
-    console.log(`Assertion Failed:⛔️⛔️⛔️  ${actual} !==  ${expected}`);
-  }
-
-};
-
-
 const tail = function(array) {
 
   if (Array.isArray(array) && array.length > 0) {
@@ -18,14 +8,4 @@ const tail = function(array) {
   return [];
 };
 
-assertEqual(tail([]), []);
-assertEqual(tail([1]), []);
-assertEqual(tail(), []);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-const result2 = tail(['a', 'b', 'c']);
-assertEqual(result2.length, 2);
-assertEqual(result2[0], "b");
-assertEqual(result2[1], "c");
+module.exports = tail;
