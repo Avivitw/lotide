1547ea7096d1b00f463623937dfb3fed89b94641
Avivit Weissman <wavivit@gmail.com>
2021-05-03 23:53:48 +0000

    refactor the code deleted console.log that where for debugging

--- middle.js
+++ middle.js
@@ -3,16 +3,13 @@ const middle = function(array) {
   //check if the length of array is odd or even
 
   const arrLength = array.length;
-  console.log('arrLength', arrLength);
   if (arrLength <= 2) {
     return [];
   } else if (arrLength % 2 !== 0) {
     let midd = Math.floor(arrLength / 2);
-    console.log('middOdd', midd);
     return array[midd];
   } else {
     let midd = (arrLength / 2) - 1;
-    console.log('middEven', midd);
     return array.slice(midd, midd + 2);
 
   }
