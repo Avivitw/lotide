c1c93b95ea4daf8e2b61a12d122035a65c2bca73
Avivit Weissman <wavivit@gmail.com>
2021-05-03 20:16:08 +0000

    removed the tests and export module assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+// const { Module } = require("node:module");
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed:✅✅✅   ${actual}  ===  ${expected}`);
@@ -7,9 +9,4 @@ const assertEqual = function(actual, expected) {
 
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("vivi", "vivi");
-assertEqual("abc", "def");
-assertEqual(123, 123);
-assertEqual(123, 456);
+module.exports = assertEqual;
