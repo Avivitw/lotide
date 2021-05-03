bc8303ba60d2d8feac99c7182625f392133c1b26
Avivit Weissman <wavivit@gmail.com>
2021-05-03 20:17:51 +0000

    Added a test folder for all my tests for lotide

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("vivi", "vivi");
+assertEqual("abc", "def");
+assertEqual(123, 123);
+assertEqual(123, 456);
