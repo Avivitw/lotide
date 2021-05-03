5562bb0dcb273cbeda29d5873094ae13715b0d33
Avivit Weissman <wavivit@gmail.com>
2021-05-03 21:17:20 +0000

    removed the tests and export module middle

--- middle.js
+++ middle.js
@@ -1,5 +1,5 @@
 const middle = function(array) {
-  //check the length of tha array if length is <2 return an empty array
+  //check the length of tha array if length is < 2 return an empty array
   //check if the length of array is odd or even
 
   const arrLength = array.length;
@@ -18,50 +18,5 @@ const middle = function(array) {
   }
 };
 
-// console.log(middle([1, 2, 3, 4, 5, 6]));
-// console.log(middle([1, 2, 3, 4, 5]));
-// console.log(middle([1, 2]));
 
-
-
-
-
-
-const eqArrays = function(arr1, arr2) {
-  if (!arr1.length === arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-    
-  }
-  return true;
-
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (!eqArrays(actual, expected)) {
-    console.log(`Assertion Failed:⛔️⛔️⛔️  ${actual} !==  ${expected}`);
-  } else {
-    console.log(`Assertion Passed:✅✅✅  ${actual}  ===  ${expected}`);
-   
-  }
-
-};
-
-
-const nums = [1, 2, 3, 4, 5];
-let numsMiddle = middle(nums);
-assertArraysEqual(numsMiddle, [3]);
-
-const nums1 = [1, 2, 3, 4, 5, 6];
-let numsMiddle1 = middle(nums1);
-assertArraysEqual(numsMiddle1, [3,4]);
-
-
-const one = [1];
-let oneMiddle = middle(one);
-assertArraysEqual(oneMiddle, []);
+module.exports = middle;
