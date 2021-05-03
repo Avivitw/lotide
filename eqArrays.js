96101dc2f9a6b47f750780b9320e3b3cb0011eec
Avivit Weissman <wavivit@gmail.com>
2021-05-03 20:33:30 +0000

    removed the tests and export module eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed:✅✅✅ " +  actual + "  ===  " + expected);
-  } else {
-    console.log(`Assertion Failed:⛔️⛔️⛔️  ${actual} !==  ${expected}`);
-  }
-
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -22,10 +13,5 @@ const eqArrays = function(arr1, arr2) {
 
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
 
