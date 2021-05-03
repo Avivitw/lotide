580c760bbb6c205e503350e82032d16fd5ed3e94
Avivit Weissman <wavivit@gmail.com>
2021-05-03 21:47:23 +0000

    change the file to use mocha frame-work and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,25 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
-assertEqual(head(123), undefined);
+describe("#head", () => {
+  it("should returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("should returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it(`should returns "Labs" for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("should returns undefined for []", () => {
+    assert.strictEqual((head([]), undefined));
+  });
+
+  it("should returns undefined for (123)",  () => {
+    assert.strictEqual((head(123), undefined));
+  });
+
+});
